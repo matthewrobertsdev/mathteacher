ca4b26ce6c63ab2079b897cb69963c023139f28d
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-07-21 13:30:16 -0400

    Can now verify a user's password, but no jwt yet.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,38 +1,38 @@
-const bodyParser=require('body-parser')
-const Recents=require('../models/recents').recents
-const Page=require('../models/page').page
-const jwt=require('jsonwebtoken')
-const User=require('../models/user').user
-module.exports=function(app){
+const bodyParser = require('body-parser')
+const Recents = require('../models/recents').recents
+const Page = require('../models/page').page
+const jwt = require('jsonwebtoken')
+const User = require('../models/user').user
+module.exports = function (app) {
   app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(bodyParser.urlencoded({ extended: true }))
 
   //get for a main teaching page
-  app.get('/teachings/:teachingName', function(req, res){
+  app.get('/teachings/:teachingName', function (req, res) {
     console.log('teaching page history update')
     console.log(req.params.teachingName)
     updateRecents(req.params.teachingName, null, null, res)
   })
 
   //get for a method teaching
-  app.get('/teachings/:teachingName/:methodName', function(req, res){
+  app.get('/teachings/:teachingName/:methodName', function (req, res) {
     console.log('method page history update')
     console.log(req.params.teachingName)
     updateRecents(req.params.teachingName, req.params.methodName, null, res)
   })
 
   //get for a solving page
-  app.get('/teachings/:teachingName/:methodName/:args', function(req, res){
+  app.get('/teachings/:teachingName/:methodName/:args', function (req, res) {
     console.log('solving page history update')
     console.log(req.params.teachingName)
     updateRecents(req.params.teachingName, req.params.methodName, req.params.args, res)
   })
   //get for the recents page
-  app.get('/recents', function(req, res){
+  app.get('/recents', function (req, res) {
     console.log('Recieved request for recents')
     //res.json({message: 'Recieved request for recents'})
     Recents.find({}, function (err, docs) {
-      if (docs.length!=0){
+      if (docs.length != 0) {
         res.json(docs[0])
       } else {
         res.json({})
@@ -40,66 +40,89 @@ module.exports=function(app){
     })
   })
 
-  app.post('/login', function(req, res){
-    User.findOne({'email': req.body.email}, function (err, user){
-      console.log('user found')
-    })
-  })
-
-  app.post('/createAccount', function(req, res){
+  app.post('/login', function (req, res) {
     console.log(req.body)
-    if (req.body.email===null ||req.body.email===undefined||req.body.email===""){
-      res.json({error: 'email must not be blank'})
+    if (req.body.email === null || req.body.email === undefined || req.body.email === "") {
+      res.json({ error: 'email must not be blank' })
       return
     }
-    if (req.body.password===null ||req.body.password===undefined||req.body.password===""){
-      res.json({error: 'password must not be blank'})
+    if (req.body.password === null || req.body.password === undefined || req.body.password === "") {
+      res.json({ error: 'password must not be blank' })
       return
     }
-    User.find({'email': req.body.email}, function (err, docs){
-      if (docs.length==0){
-        console.log('email not taken')
-        res.json({success: true})
-        return
+    handlePassword = (bool)=>{
+      if (bool) {
+        res.json({ success: true })
       } else {
-        console.log('email is taken')
-        res.json({error: 'email is taken'})
-        return
+        res.json({ error: 'invalid username or password' })
       }
+    }
+    User.find({ 'email': req.body.email }, function (err, docs) {
+      if (docs.length == 0) {
+        res.json({ error: 'invalid username or password' })
+      } 
+      docs[0].isCorrectPassword(req.body.password, handlePassword)
     })
+})
+
+app.post('/createAccount', function (req, res) {
+  if (req.body.email === null || req.body.email === undefined || req.body.email === "") {
+    res.json({ error: 'email must not be blank' })
+    return
+  }
+  if (req.body.password === null || req.body.password === undefined || req.body.password === "") {
+    res.json({ error: 'password must not be blank' })
+    return
+  }
+  User.find({ 'email': req.body.email }, function (err, docs) {
+    if (docs.length == 0) {
+      console.log('email not taken')
+      const user = User({
+        email: req.body.email,
+        password: null,
+        salt: null
+      })
+      user.savePassword(req.body.password)
+      res.json({ success: true })
+      return
+    } else {
+      console.log('email is taken')
+      res.json({ emailTaken: true })
+      return
+    }
   })
+})
 
 }
 
-function updateRecents(teaching, methodName, args, res){
+function updateRecents(teaching, methodName, args, res) {
   Recents.find({}, function (err, docs) {
     console.log(err)
-    console.log(teaching)
     //create the page
-    const page=Page({
+    const page = Page({
       teachingName: teaching,
-        method: methodName,
-        arguments: args
+      method: methodName,
+      arguments: args
     })
     //if recents already exists
-    if (docs.length!=0){
+    if (docs.length != 0) {
       console.log('recents found')
-      const doc=docs[0]
-      let index=-1
+      const doc = docs[0]
+      let index = -1
       //try to find an index of a duplicate recents item
-      for (let i=0; i<doc.pages.length; i++){
-        if (doc.pages[i].teachingName==teaching
-          &&doc.pages[i].method==methodName
-          &&doc.pages[i].arguments==args){
-            index=i
-          }
+      for (let i = 0; i < doc.pages.length; i++) {
+        if (doc.pages[i].teachingName == teaching
+          && doc.pages[i].method == methodName
+          && doc.pages[i].arguments == args) {
+          index = i
+        }
       }
       //remove if already there
-      if (index!==-1){
+      if (index !== -1) {
         doc.pages.splice(index, 1)
       }
       //get rid of oldest
-      if (doc.pages.length>99){
+      if (doc.pages.length > 99) {
         docs.pages.pop()
       }
       //add newest
@@ -107,18 +130,18 @@ function updateRecents(teaching, methodName, args, res){
       //update the doc in the db
       doc.save();
       //send the doc over for debugging
-      res.json({messsage: 'Recent saved'})
+      res.json({ messsage: 'Recent saved' })
     } else {
       //if recents is not found
       console.log('recents not found')
       //create the recents with the new page
-      const recents=Recents({
+      const recents = Recents({
         pages: [page]
       })
       //save the recents
-      recents.save(function(err){
+      recents.save(function (err) {
         console.log(err)
-        res.json({message: 'Recents initiated with first page'})
+        res.json({ message: 'Recents initiated with first page' })
         console.log('Recents initiated')
       })
     }
--- models/user.js
+++ models/user.js
@@ -3,19 +3,41 @@ const crypto=require('crypto')
 
 const Schema=mongoose.Schema
 
-const userSchema=new Schema({
+let userSchema=new Schema({
   email: String,
   password: String,
   salt: String,
 })
 
 userSchema.methods.savePassword=function(password){
+  let user=this
+  console.log(user)
   this.salt=crypto.randomBytes(128).toString('base64')
-  this.password=crypto.pbkdf2(password, salt, 10000)
+  crypto.pbkdf2(password, this.salt, 10000, 64, 'sha512', (err, key)=>{
+    if (err){
+      console.log(err)
+      throw err;
+    }
+    console.log(key.toString('base64'))
+    user.password=key.toString('base64')
+    console.log(user)
+    user.save()
+  })
 }
 
-userSchema.methods.isCorrectPassword=function(attemptedPassword){
-  return this.password==crypto.pbkdf2(attemptedPassword, salt, 10000)
+userSchema.methods.isCorrectPassword=function(attemptedPassword, callback){
+  let user=this
+  console.log(user)
+  crypto.pbkdf2(attemptedPassword, this.salt, 10000, 64, 'sha512', (err, key)=>{
+    if (err){
+      console.log(err)
+      throw err;
+    }
+    console.log("1234"+key.toString('base64'))
+    console.log(user.password)
+    console.log(user.password==key.toString('base64'))
+    callback(user.password==key.toString('base64'))
+  })
 }
 
 const User=mongoose.model('User', userSchema)
